2e65283ae08d7ffd7cb267d8d860b0963a3b8551
ltopic764 <ltopic764@gmail.com>
2026-05-29 17:59:09 +0200

    Add matchmaking with RNGCryptoServiceProvider
    Created helper classes when calculating a score between the person and a candidate to which we want to send the letter, also added main logic listing all candidates and seeing the one that is the best match

---
+++ PubSubCupid/PubSubCupid.Core/Matching/CupidMatchmaker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PubSubCupid.Core.Models;
+
+namespace PubSubCupid.Core.Matching
+{
+    public static class CupidMatchmaker
+    {
+        // Pronalazimo najbolji match
+        public static MatchScore FindBestMatch(SinglePerson person, IEnumerable<SinglePerson> allPeople)
+        {
+            MatchScore bestMatch = null;
+
+            foreach (SinglePerson candidate in allPeople)
+            {
+                // Ne sme slati sam sebi pisma
+                if (candidate.Username == person.Username)
+                {
+                    continue;
+                }
+
+                // Da li je neko od svih ljudi u sistemu blokiran
+                if (person.BlockedUsernames.Contains(candidate.Username))
+                {
+                    continue;
+                }
+
+                // Cekamo da korisnik potvrdi da je primio pismo
+                if (candidate.WaitingForConfirmation)
+                {
+                    continue;
+                }
+
+                int score = CupidScoring.CalculateScore(person, candidate);
+
+                if (bestMatch == null || score > bestMatch.Score)
+                {
+                    bestMatch = new MatchScore { Candidate = candidate, Score = score };
+                } 
+            }
+
+            return bestMatch;
+        }
+    }
+}
