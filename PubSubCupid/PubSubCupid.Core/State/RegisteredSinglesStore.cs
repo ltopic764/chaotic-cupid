1efd656eff61f51697272ebf59f223ad5dac4938
ltopic764 <ltopic764@gmail.com>
2026-05-29 14:42:13 +0200

    Models
    Add core domain models

---
+++ PubSubCupid/PubSubCupid.Core/State/RegisteredSinglesStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using PubSubCupid.Core.Models;
+
+namespace PubSubCupid.Core.State
+{
+    public class RegisteredSinglesStore
+    {
+        // Sve trenutno prijavljene osobe
+        private readonly List<SinglePerson> _registeredSingles;
+
+        public RegisteredSinglesStore()
+        {
+            _registeredSingles = new List<SinglePerson>();
+        }
+
+        public IReadOnlyCollection<SinglePerson> GetAll()
+        {
+            return _registeredSingles.AsReadOnly();
+        }
+
+        public bool UsernameExists(string username)
+        {
+            return _registeredSingles.Any(person => person.Username == username);
+        }
+
+        public void Add(SinglePerson person)
+        {
+            _registeredSingles.Add(person);
+        }
+
+        public SinglePerson FindByUsername(string username)
+        {
+            return _registeredSingles.FirstOrDefault(person => person.Username == username);
+        }
+    }
+}
