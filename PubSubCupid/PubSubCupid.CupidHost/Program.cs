46fecc0b079acc7532e5d585bee411f87feedfe0
ltopic764 <ltopic764@gmail.com>
2026-05-30 13:21:37 +0200

    Add WCF service implementation and host

--- PubSubCupid/PubSubCupid.CupidHost/Program.cs
+++ PubSubCupid/PubSubCupid.CupidHost/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using PubSubCupid.CupidHost.Services;
 
 namespace PubSubCupid.CupidHost
 {
@@ -10,6 +12,28 @@ namespace PubSubCupid.CupidHost
     {
         static void Main(string[] args)
         {
+            ChaoticCupidService service = new ChaoticCupidService();
+
+            ServiceHost host = new ServiceHost(service);
+
+            try
+            {
+                host.Open();
+
+                Console.WriteLine("Cupid service started");
+
+                Console.WriteLine("Press ENTER to stop");
+
+                Console.ReadLine();
+
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                host.Abort();
+            }
         }
     }
 }
